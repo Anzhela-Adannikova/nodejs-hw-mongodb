6798d7ceb92dbbed6b470fed4d060ce0634f341a
Anzhela Adannikova <anzhelaadannikova@gmail.com>
2025-09-06 13:29:21 +0300

    step 7

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { notFoundHandler } from './middlewares/notFoundHandler.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import cookieParser from 'cookie-parser';
 import { UPLOAD_DIR } from './constants/index.js';
+import { swaggerDocs } from './middlewares/swaggerDocs.js';
 
 dotenv.config();
 
@@ -28,6 +29,7 @@ const setupServer = () => {
   app.use(json());
 
   app.use('/uploads', express.static(UPLOAD_DIR));
+  app.use('/api-docs', swaggerDocs);
 
   app.use(router);
 
